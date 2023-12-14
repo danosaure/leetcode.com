f0d802afec11f5e7a17206b8873620304037048a
Danosaure <1444063+danosaure@users.noreply.github.com>
2023-12-14 18:52:44 -0500

    Fixed tab size

--- src/009-palindrome-number/is-palindrome.js
+++ src/009-palindrome-number/is-palindrome.js
@@ -3,24 +3,24 @@
  * @return {boolean}
  */
 module.exports = (x) => {
-    if (x < 0) {
-        return false;
-    } else if (x < 10) {
-        return true;
-    }
-    const s = '' + x;
-
-    len = s.length;
-
-    if (s[len-1] === '0') {
-        return false;
-    }
-
-    limit = Math.floor(len/2);
-    for (i = 0; i < limit; i++) {
-        if (s[i] !== s[len - i - 1]) {
-            return false;
-        }
-    }
+  if (x < 0) {
+    return false;
+  } else if (x < 10) {
     return true;
+  }
+  const s = '' + x;
+
+  len = s.length;
+
+  if (s[len - 1] === '0') {
+    return false;
+  }
+
+  limit = Math.floor(len / 2);
+  for (i = 0; i < limit; i++) {
+    if (s[i] !== s[len - i - 1]) {
+      return false;
+    }
+  }
+  return true;
 };
--- src/009-palindrome-number/is-palindrome.unit.test.js
+++ src/009-palindrome-number/is-palindrome.unit.test.js
@@ -3,19 +3,19 @@ const expect = require('chai').expect;
 const isPalindrome = require('./is-palindrome');
 
 describe(__filename, () => {
-    it('Case 1', () => {
-        expect(isPalindrome(121)).to.be.true();
-    })
+  it('Case 1', () => {
+    expect(isPalindrome(121)).to.be.true();
+  });
 
-    it('Case 2', () => {
-        expect(isPalindrome(-121)).to.be.false();
-    })
+  it('Case 2', () => {
+    expect(isPalindrome(-121)).to.be.false();
+  });
 
-    it('Case 3', () => {
-        expect(isPalindrome(10)).to.be.false();
-    })
+  it('Case 3', () => {
+    expect(isPalindrome(10)).to.be.false();
+  });
 
-    it('With 0', () => {
-        expect(isPalindrome(0)).to.be.true();
-    })
-})
+  it('With 0', () => {
+    expect(isPalindrome(0)).to.be.true();
+  });
+});
