785bdc52ab28972e6286eccce5bebfbb02944d8a
Danosaure <1444063+danosaure@users.noreply.github.com>
2023-12-15 01:17:30 -0500

    Fixed eslint errors

--- src/009-palindrome-number/is-palindrome.js
+++ src/009-palindrome-number/is-palindrome.js
@@ -10,14 +10,14 @@ module.exports = (x) => {
   }
   const s = '' + x;
 
-  len = s.length;
+  const len = s.length;
 
   if (s[len - 1] === '0') {
     return false;
   }
 
-  limit = Math.floor(len / 2);
-  for (i = 0; i < limit; i++) {
+  const limit = Math.floor(len / 2);
+  for (let i = 0; i < limit; i++) {
     if (s[i] !== s[len - i - 1]) {
       return false;
     }
